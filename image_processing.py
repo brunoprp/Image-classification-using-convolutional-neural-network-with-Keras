acc1370c25545addb240d7873145d4d2410e9f0b
Bruno <domingosbruno4764@gmail.com>
2019-09-19 17:14:41 -0300

    comentes

--- image_processing.py
+++ image_processing.py
@@ -6,41 +6,43 @@ Created on Wed Sep 18 18:42:43 2019
 @author: bruno
 """
 
-import os
-import cv2
-from tqdm import tqdm
-import random
-import numpy as np
-import keras
+import os #Read directories
+import cv2 #Read the pictures
+from tqdm import tqdm #Graphically show the progress of the 'FOR'
+import random #Randomize the images
+import numpy as np #Array work
+import keras #Format the arrays
 
+############### Creating a class to load and process images!######################
 
 class ImageProcessing():
-    def __init__(self):
+    def __init__(self):  #creating builder
         self.images = []
         self.classe = []
 
 
-    def loadImages(self, diretory, sizeImg, channels = 0):
+    def loadImages(self, diretory, sizeImg, channels = 0):#receive the directory the images the size they will be read and number of channels
         """  Load Imges  """
-        direImages = os.listdir(diretory)
-        direImages = random.sample(direImages, len(direImages))
-        for i in tqdm(direImages):
-            if i != '.DS_Store':
-                name = i.split('_')[0]
-                img = cv2.imread(diretory+i, channels)
-                img = cv2.resize(img,(sizeImg,sizeImg))
-                img = img.astype('float32')/255
-                if name == 'normal':
+        direImages = os.listdir(diretory)#list everything in the directory
+        direImages = random.sample(direImages, len(direImages))#Randomize images within directory
+        for i in tqdm(direImages):#go through the directory
+            if i != '.DS_Store':#ignore not image file inside directory
+                name = i.split('_')[0]#capture image name (Normal or Pneumonia)
+                img = cv2.imread(diretory+i, channels)#Load image
+                img = cv2.resize(img,(sizeImg,sizeImg))# resize images
+                img = img.astype('float32')/255 #Normalize image values
+                #If the image name is normal the class is 0 if not the class is 1
+                if name == 'normal': 
                     self.images.append(img)
                     self.classe.append(0)
                 else:
                     self.images.append(img)
                     self.classe.append(1)
-        images =  np.array(self.images)
-        classe = np.array(self.classe)
+        images =  np.array(self.images)#Array numpy
+        classe = np.array(self.classe)#Array numpy
 
-        images = images.reshape(images.shape[0], sizeImg,sizeImg,1)
-        classe = keras.utils.to_categorical(classe,2)
+        images = images.reshape(images.shape[0], sizeImg,sizeImg,1)#CNN input format (x,sizeImge,sizeImge,channels)
+        classe = keras.utils.to_categorical(classe,2)#CNN input format class
 
-        return images,classe
+        return images,classe #ruturn images and class
 
--- training_CNN.py
+++ training_CNN.py
@@ -7,57 +7,62 @@ Created on Wed Sep 18 20:23:06 2019
 """
 
 
-import image_processing
+import image_processing #import the image processing script
 
-dire_train = 'data/train/'
-dire_test = 'data/test/'
-dire_val = 'data/val/'
+dire_train = 'data/train/' #trainig diretory
+dire_test = 'data/test/' #test diretory
 
+##########Reading the test and training images!############
+sizeImg = 200 #image dimension
+images = image_processing.ImageProcessing() #Instantiating an object from class (ImageProcessing)
 
-sizeImg = 200
+x_train, y_train = images.loadImages(dire_train,sizeImg) #Trainig image processing 
+x_test, y_test = images.loadImages(dire_test,sizeImg) #Test imge processing
 
-images = image_processing.ImageProcessing()
+######## Creating the model of convolutional neural network ################
 
-x_train, y_train = images.loadImages(dire_train,sizeImg)
-x_test, y_test = images.loadImages(dire_test,sizeImg)
-
-
+"""Importing the Keras library for model building"""
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 
-from keras.optimizers import Adam
-EPOCHS = 20
-INIT_LR = 1e-3
-BS = 40
-opt = Adam(lr=INIT_LR, decay=INIT_LR / 100)
+"""Setting Network Hyperparameters"""
+EPOCHS = 20 #Epocha number
+INIT_LR = 1e-3 #Learning rate
+BS = 40 # batch size, amount of images that will pass through the network in each season
+opt = Adam(lr=INIT_LR, decay=INIT_LR / 100) #optimizer
 
-#create model
-model = Sequential()
+model = Sequential() # Sequential model keras
 #add model layers
-model.add(Conv2D(64, kernel_size = 3, activation = 'relu', input_shape=(sizeImg,sizeImg,1)))
+"""Convolutional layers"""
+model.add(Conv2D(64, kernel_size = 3, activation = 'relu', input_shape=(sizeImg,sizeImg,1))) #
 model.add(Conv2D(32, kernel_size = 3, activation = 'relu'))
 model.add(Conv2D(16, kernel_size = 3, activation = 'relu'))
 model.add(Flatten())
-
+"""Dense layers"""
 model.add(Dense(16, activation='relu'))
 model.add(Dense(8, activation='relu'))
-model.add(Dense(2, activation='softmax'))
-model.compile(optimizer = opt, loss='categorical_crossentropy',metrics=['accuracy'])
-model.summary()
+model.add(Dense(2, activation='softmax'))# the last layer must have the same number of classes
+model.compile(optimizer = opt, loss='categorical_crossentropy',metrics=['accuracy']) #Compiling the model by defining the optimizer the loss function and the evaluation metric
+model.summary()#Print CNN architecture
 
-filepath='model.h5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+#########Training CNN#######
+
+"""Save model treined"""
+filepath='model.h5'#name model save
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')#salve best model
 callbacks_list = [checkpoint]
 
+"""here a keras technique is used to improve training accuracy, the images are molded for the network to learn better"""
 aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
-	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-	horizontal_flip=True, fill_mode="nearest")
+    height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+    horizontal_flip=True, fill_mode="nearest")
 
+"""CNN trained by passing training and test data jutamnete with hyperparameters"""
 history = model.fit_generator(
-	aug.flow(x_train, y_train, batch_size=BS),
-	validation_data=(x_test, y_test),
-	steps_per_epoch=len(x_train) // BS,
-	epochs=EPOCHS, verbose=1, callbacks = callbacks_list)
+    aug.flow(x_train, y_train, batch_size=BS),
+    validation_data=(x_test, y_test),
+    steps_per_epoch=len(x_train) // BS,
+    epochs=EPOCHS, verbose=1, callbacks = callbacks_list)
